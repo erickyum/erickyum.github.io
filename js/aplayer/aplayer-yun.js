f1025f34bcf3d675ecd7f41515b20b9013d839d0
erickyum <2410916019@qq.com>
2023-06-04 22:40:48 +0800

    Site updated: 2023-06-04 22:40:41

--- js/aplayer/aplayer-yun.js
+++ js/aplayer/aplayer-yun.js
@@ -62,9 +62,9 @@ var yun = {
 // 音乐绑定事件
 function musicBindEvent() {
 	document.querySelector("#nav-music .aplayer-music").addEventListener("click", function() {
-		anzhiyu.musicTelescopic();
+		yun.musicTelescopic();
 	});
 	document.querySelector("#nav-music .aplayer-button").addEventListener("click", function() {
-		anzhiyu.musicToggle(false);
+		yun.musicToggle(false);
 	});
 }
